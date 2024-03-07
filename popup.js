c10b146e28f8ff99407e8c58bc57a79850766483
codfy-ai <149323047+codfy-ai@users.noreply.github.com>
2024-03-07 11:50:11 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,17 +1,3 @@
-const readingTimeElement = document.getElementById('readingTime');
-const toggleButton = document.getElementById('toggleButton');
-const bypassStatusElement = document.getElementById('bypassStatus');
-
-const article = document.querySelector("article");
-if (article) {
-  const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g;
-  const words = text.matchAll(wordMatchRegExp);
-  const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 200);
-  readingTimeElement.textContent = `⏱️ ${readingTime} min read`;
-}
-
 let bypassStatus = false; 
 toggleButton.addEventListener('click', () => {
   bypassStatus = !bypassStatus;
